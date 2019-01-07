23019e0261bde0240f3b6c29c339dcacdcf711e3
Medhat Elmasry <medhat@elmasry.ca>
2019-01-07 10:35:46 -0800

    first commit

--- server.js
+++ server.js
@@ -14,13 +14,21 @@ var app = express();
 // Environment variables
 let confPath = path.join(__dirname,'.env' );
 dotenv.config({ path: confPath });
-var port = process.env.PORT || 3000;
-var db_server = process.env.DB_SERVER || "localhost";
 
-mongoose.connect("mongodb://" + db_server + "/school", function (err) {
-   if (err) throw err;
-   console.log('Successfully connected');
-});
+var port = process.env.PORT || 3000;
+var connStr = process.env.COSMOSDB_CONNSTR;
+connStr = connStr.replace('{COSMODDB_USER}', process.env.COSMODDB_USER);
+connStr += process.env.COSMOSDB_DATABASE_NAME;
+connStr += "?ssl=true&replicaSet=globaldb";
+
+mongoose.connect(connStr, {
+      auth: {
+        user: process.env.COSMODDB_USER,
+        password: process.env.COSMOSDB_PASSWORD
+      }
+})
+.then(() => console.log('Connection to CosmosDB ' + process.env.COSMOSDB_DATABASE_NAME + ' successful'))
+.catch((err) => console.error(err));
 
 // View engine
 var ejsEngine = require("ejs-locals");
@@ -29,6 +37,14 @@ app.set("view engine", "ejs");          // ejs view engine
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "client")));
+
+// Enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");         
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");         
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+});           
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -39,5 +55,5 @@ app.use("/api", students);
 
 app.listen(port, function() {
     console.log("Server started on port: " + port);
-    console.log("Using mongo database on server: " + db_server);
+    console.log("Using mongo database on server: " + process.env.COSMOSDB_DATABASE_NAME);
 });
